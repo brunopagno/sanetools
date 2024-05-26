55c7116544469487e7afeb1c77a87d18f55c7ba0
Bruno Pagno <brunopagno@proton.me>
2024-05-26 11:06:53 +0200

    insanity cmd helper takes folder as argument

--- bin/insanity.js
+++ bin/insanity.js
@@ -16,27 +16,39 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const sourceDir = path.resolve(__dirname, "..");
-const targetDir = process.cwd();
+const targetDirName = process.argv[2];
 
-fs.readdir(sourceDir, (err, files) => {
+if (!targetDirName) {
+  console.error("Please provide a target directory");
+  process.exit(1);
+}
+
+const targetDir = path.resolve(process.cwd(), targetDirName);
+
+fs.mkdir(targetDir, (err) => {
   if (err) throw err;
+  console.log(`Directory ${targetDir} created`);
 
-  files.forEach((file) => {
-    if (ignoreList.includes(file)) return;
+  fs.readdir(sourceDir, (err, files) => {
+    if (err) throw err;
 
-    const sourceFile = path.join(sourceDir, file);
-    const targetFile = path.join(targetDir, file);
+    files.forEach((file) => {
+      if (ignoreList.includes(file)) return;
 
-    if (fs.lstatSync(sourceFile).isDirectory()) {
-      fs.cp(sourceFile, targetFile, { recursive: true }, (err) => {
-        if (err) throw err;
-        console.log(`${file} created at ${targetDir}`);
-      });
-    } else {
-      fs.copyFile(sourceFile, targetFile, (err) => {
-        if (err) throw err;
-        console.log(`${file} created at ${targetDir}`);
-      });
-    }
+      const sourceFile = path.join(sourceDir, file);
+      const targetFile = path.join(targetDir, file);
+
+      if (fs.lstatSync(sourceFile).isDirectory()) {
+        fs.cp(sourceFile, targetFile, { recursive: true }, (err) => {
+          if (err) throw err;
+          console.log(`${file} created at ${targetDir}`);
+        });
+      } else {
+        fs.copyFile(sourceFile, targetFile, (err) => {
+          if (err) throw err;
+          console.log(`${file} created at ${targetDir}`);
+        });
+      }
+    });
   });
 });
