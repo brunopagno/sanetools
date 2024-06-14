699dea5302c0e725cadee8020323bc5ae88a3246
Bruno Pagno <brunopagno@proton.me>
2024-06-14 18:06:51 +0200

    eslint I guess :shrug:

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginTs from "typescript-eslint";
+import tseslint from "typescript-eslint";
+
 
 export default [
-  { languageOptions: { globals: globals.node } },
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
-  pluginTs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
-
