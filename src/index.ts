a7ded16c3c54f7010fad4cf99190cab00bb9525b
Bruno Pagno <brunopagno@proton.me>
2024-06-14 18:08:27 +0200

    TS (#13)
    * everything works, but eslint
    
    * eslint I guess :shrug:

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import type { Request, Response } from "express";
 import dotenv from "dotenv";
 
 if (process.env.NODE_ENV !== "production") {
@@ -10,9 +11,9 @@ const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 export const app = express();
 
-app.get("/", (_req, res) => {
+app.get("/", (_req: Request, res: Response) => {
   res.setHeader("content-type", "application/json");
   res.send(JSON.stringify({ status: "healthy" }));
 });
 
-app.listen(SERVER_PORT, console.log(`Server is live at ${SERVER_PORT}`));
+app.listen(SERVER_PORT, () => console.log(`Server is live at ${SERVER_PORT}`));
